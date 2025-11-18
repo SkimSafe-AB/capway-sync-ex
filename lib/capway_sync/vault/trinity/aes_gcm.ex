25bfc7822bb4e0b5443063be24a33c77b0d7c292
Philip Mannheimer <philip.mannheimer@gmail.com>
2025-11-18 13:24:12 +0100

    updated vault with more modern config

--- lib/capway_sync/vault/trinity/aes_gcm.ex
+++ lib/capway_sync/vault/trinity/aes_gcm.ex
@@ -4,30 +4,30 @@ defmodule CapwaySync.Vault.Trinity.AES.GCM do
 
   @impl GenServer
   def init(config) do
-    if System.get_env("TRINITY_DB_VAULT_KEY") do
-      ciphers = [
-        default: {
-          Cloak.Ciphers.AES.GCM,
-          tag: aes_tag(), key: decode_env!("TRINITY_DB_VAULT_KEY")
-        }
-      ]
+    default_cipher = [
+      default: {
+        Cloak.Ciphers.AES.GCM,
+        tag: aes_tag(), key: decode_env!("TRINITY_DB_VAULT_KEY"), iv_length: iv_length()
+      }
+    ]
 
-      ciphers =
-        case System.get_env("TRINITY_DB_VAULT_KEY_RETIRED") do
-          nil -> ciphers
-          "" -> ciphers
-          env_key ->
-            retired_key = Base.decode64!(env_key)
-            Keyword.put(ciphers, :retired, {
+    retired_key = decode_env!("TRINITY_DB_VAULT_KEY_RETIRED")
+
+    ciphers =
+      if not is_nil(retired_key) and retired_key != "" do
+        default_cipher ++
+          [
+            retired: {
               Cloak.Ciphers.AES.GCM,
-              tag: aes_tag(), key: retired_key
-            })
-        end
+              tag: aes_tag_retired(), key: retired_key, iv_length: iv_length_retired()
+            }
+          ]
+      else
+        default_cipher
+      end
 
-      {:ok, Keyword.put(config, :ciphers, ciphers)}
-    else
-      {:ok, config}
-    end
+    IO.inspect(ciphers, label: "Vault Ciphers Configured")
+    {:ok, Keyword.put(config, :ciphers, ciphers)}
   end
 
   defp decode_env!(var) do
@@ -38,11 +38,45 @@ defmodule CapwaySync.Vault.Trinity.AES.GCM do
 
   defp aes_tag() do
     tag = "TRINITY_VAULT_TAG" |> System.get_env()
+    IO.puts("ENV TAG: #{tag}")
 
     if is_nil(tag) or tag == "" do
       "AES.GCM.V1"
     else
       tag
+    end
+  end
+
+  defp aes_tag_retired() do
+    tag = "TRINITY_VAULT_TAG_RETIRED" |> System.get_env()
+    IO.puts("ENV TAG RETIRED: #{tag}")
+
+    if is_nil(tag) or tag == "" do
+      "AES.GCM.V1"
+    else
+      tag
+    end
+  end
+
+  defp iv_length() do
+    length = "TRINITY_VAULT_IV_LENGTH" |> System.get_env()
+
+    if is_nil(length) or length == "" do
+      # Default IV length for AES.GCM
+      12
+    else
+      String.to_integer(length)
+    end
+  end
+
+  defp iv_length_retired() do
+    length = "TRINITY_VAULT_IV_LENGTH_RETIRED" |> System.get_env()
+
+    if is_nil(length) or length == "" do
+      # Default IV length for AES.GCM
+      12
+    else
+      String.to_integer(length)
     end
   end
 end
