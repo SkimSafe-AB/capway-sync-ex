fd271b4531fbd483c2a0a5568074c504d39cb6a4
Philip Mannheimer <philip.mannheimer@gmail.com>
2026-06-23 11:49:39 +0200

    added capway sync

--- lib/capway_sync/application.ex
+++ lib/capway_sync/application.ex
@@ -29,7 +29,8 @@ defmodule CapwaySync.Application do
       "SYNC_REPORTS_TABLE",
       "ACTION_ITEMS_TABLE",
       "CAPWAY_CACHE_TABLE",
-      "CAPWAY_CONTRACTS_TABLE"
+      "CAPWAY_CONTRACTS_TABLE",
+      "CAPWAY_CREDITOR"
     ]
     |> Enum.each(fn env ->
       System.get_env(env) || raise("#{env} is not set")
